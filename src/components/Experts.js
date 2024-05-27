19b612352eaf6b19aa7ef8af9ef721c5e0524b2d
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-27 20:10:50 +0530

    experts

--- src/components/Experts.js
+++ src/components/Experts.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import img from "../assets/img.jpg"
 
 const Experts = () => {
     return (
-        <div>
-            Experts
+        <div className='max-w-[1240px] mx-autox my-10 border border-black grid grid-cols-3'>
+            <div className='border border-[blue] col-span-1'>
+                <img src={img.jpg} alt="" />
+            </div>
+            <div className='border border-[blue] col-span-2'></div>
         </div>
     );
 }
