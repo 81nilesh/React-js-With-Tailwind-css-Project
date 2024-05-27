03adbb3d2a62bb32b9ed4c64ccb8e708efa7fca2
Nilesh choudhary <121803888+81nilesh@users.noreply.github.com>
2024-05-27 21:07:00 +0530

    NewsLatter Prt completed

--- src/components/NewsLatter.js
+++ src/components/NewsLatter.js
@@ -3,13 +3,18 @@ import React from 'react';
 const NewsLatter = () => {
     return (
         <div className='bg-[#2699fb] p-4'>
-            <div className='max-w-[1240px] mx-auto flex justify-between py-[40px]'>
-                <div className=''>
+            <div className='max-w-[1240px] mx-auto md:flex justify-between py-[40px]'>
+                <div>
                     <h1 className='text-[40px] font-bold text-white'>Want to learn latest I.T skills?</h1>
                     <span className='text-white'>Sign up to our newslatter and stay up to date</span>
                 </div>
-                <div className='border border-black'>
-                    hello
+                <div>
+                    <input type="text" className='rounded mb-2 p-3 mr-2 text-[grey]' placeholder="Email" />
+                    <button className='bg-black text-white p-3 rounded'>Get Started</button>
+                    <br />
+                    <p className='p-3 text-white'>
+                        We care about the protection of your data. <br /> <a href="" className='text-black'> Read our Privacy Policy.</a>
+                    </p>
                 </div>
             </div>
         </div>
